f9c4fe0fbf6c53e7d36c3d6ea947eefa7a58561a
Olena1iashenko <lyashenko.alenkaa@gmail.com>
2024-04-20 14:01:39 +0300

    swiper fixes!!!

--- src/js/projects.js
+++ src/js/projects.js
@@ -10,12 +10,30 @@ new Swiper('.projects-swiper', {
   // Install modules
   modules: [Navigation],
   speed: 500,
+  slidesPerView: 1,
   navigation: {
     prevEl: '.js-btn_arrow-left',
     nextEl: '.js-btn_arrow-right',
     // allowSlidePrev: true,
     // allowSlideNext: true,
   },
+  breakpoints: {
+    320: {
+      // direction: 'vertical',
+      height: 288,
+    },
+    375: {
+      // direction: 'vertical',
+      height: 343,
+    },
+    768: {
+      // direction: 'vertical',
+      height: 625,
+    },
+    1440: {
+     direction: 'horizontal',
+    }
+  }
 
   // ...
 });
