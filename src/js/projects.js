9f762984f638614c09d26055ba09c97deb93a05f
Kosdevua <konstantin.vodolozhko@gmail.com>
2024-04-23 10:58:03 +0300

    Update projects.js

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,14 +1,27 @@
-import Swiper from 'swiper';
-import { Navigation } from 'swiper/modules';
+import Swiper from 'swiper/bundle';
+// import { Navigation } from 'swiper/modules';
 
-import 'swiper/css';
-import 'swiper/css/navigation';
+// import 'swiper/css';
+import 'swiper/css/bundle';
+// import 'swiper/css/navigation';
 
 new Swiper('.projects-swiper', {
   // Install modules
-  modules: [Navigation],
-  speed: 500,
+  // modules: [Navigation],
+  // speed: 500,
   slidesPerView: 1,
+  spaceBetween: 30,
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+    tabKeys: true,
+  },
+  mousewheel: {
+    invert: true,
+  },
+
   navigation: {
     prevEl: '.js-btn_arrow-left',
     nextEl: '.js-btn_arrow-right',
